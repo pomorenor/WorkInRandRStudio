2460cf9c61fae78d37b4fa0a6664c1abc01a04f7
Pohl Moreno <pomorenor@unal.edu.co>
2023-12-04 14:47:22 -0500

    Added peama and started coupling names of different sheets

--- finalScript.R
+++ finalScript.R
@@ -1,20 +1,12 @@
 library("dplyr")
 library("readxl")
 library("writexl")
+library("stringdist")
 
 rawData <- read_xlsx("PrincipalData.xlsx")
 preparedData <- read_xlsx("2023-2ToFill.xlsx")
 consolidado <- read_xlsx("Consolidado.xlsx")
 
-
-namesRawData <- rawData$PPAL_NOMPRS
-namesPreparedData <- preparedData$`APELLIDOS Y NOMBRES`
-
-#Now we convert all of them to lower case letter so we can compare them
-#However we don't do it for raw data because we have to filter it first
-
-#lowercase_rawDataNames <- tolower(namesRawData)
-lowercase_preparedDataNames <- tolower(namesPreparedData)
 
 #First we are gonna establish the filters, we start by obtaining the
 #unique names of the prof. activities 
@@ -84,14 +76,87 @@ for (value in unique(rawData$PPAL_NOMPRS)){
   }
 }                     
 
+
+# Now we search for PEAMA
+
+for (value in unique(rawData$PPAL_NOMPRS)){
+  peamaPerProf <- consolidado %>%
+    select(DOCENTE, `CURSO PEAMA`) %>%
+    filter( !is.na(`CURSO PEAMA`)) %>%
+    group_by(DOCENTE) %>%
+    summarise(PEAMA_count = sum(`CURSO PEAMA` == "PEAMA"))
+}
+
+
+peamaPerProf 
+
+SummaryProfNames <- tolower(SummarizedProfInfo$NOMBRES.Y.APELLIDOS)
+PeamaProfNames <- tolower(peamaPerProf$DOCENTE)
+
+EqualIndices <- c()
+
+for (name in PeamaProfNames){
+  matches <- stringdist::amatch(name, SummaryProfNames, method = "jaccard")
+  EqualIndices <- c(EqualIndices, matches)
+}
+
+FinalEqualIndices <- EqualIndices[!is.na(EqualIndices)]
+
+columnPeama <- vector("numeric", length = length(SummarizedProfInfo$NOMBRES.Y.APELLIDOS) )
+
+for (i in 1:length(columnPeama)){
+    columnPeama[i] <- NA
+}
+
+for (value in FinalEqualIndices){
+  columnPeama[value] <- peamaPerProf$PEAMA_count[value]
+} 
+
+columnPeama
+
+finalDT <- data.frame(SummarizedProfInfo$NOMBRES.Y.APELLIDOS, SummarizedProfInfo$HORAS.PREGRADO.2023.1, PEAMA = columnPeama)
+
+finalDT
+
 ## Now we cross our summarized information with what we've got in the form to fill 
+## For that we have to compare the names in both lists 
+
+namesRawData <- tolower(SummarizedProfInfo$NOMBRES.Y.APELLIDOS)
+namesPreparedData <- tolower(preparedData$`APELLIDOS Y NOMBRES`)
+
+vec_indices_comparison <- c()
+
+for (name in namesPreparedData) {
+  matches <- stringdist::amatch(name,  namesRawData, method = "jaccard",   maxDist = 1)
+  vec_indices_comparison <- c(vec_indices_comparison, matches)
+}
+
+final_vec_indices_comparison <- vec_indices_comparison[!is.na(vec_indices_comparison)]
+final_vec_indices_comparison
 
 
 
+#preparedData$`APELLIDOS Y NOMBRES`[[1]]
+namesRawData[[439]]
+#SummarizedProfInfo[539,  ]
+
+
+totalNombresProf <- namesRawData[final_vec_indices_comparison]
+totalHorasPregradoPorProf <- SummarizedProfInfo$HORAS.PREGRADO.2023.1[final_vec_indices_comparison]
+
+finalDF <- data.frame(totalNombresProf, totalHorasPregradoPorProf)
 
 ### Finally we print to an excel file   
 
+finalDF[3, ]
 
 excel_output <- "SumInfo.xlsx"
-write_xlsx(SummarizedProfInfo, excel_output)
+write_xlsx(finalDF, excel_output)
+write_xlsx(SummarizedProfInfo, "pato.xlsx")
+write_xlsx(finalDT, "dataConPeama.xlsx")
 
+
+string1 <- tolower("Pablo Enrique Abril Contreras")
+string2 <- tolower("ABRIL CONTRERAS PABLO ENRIQUE")
+distance <- stringdist::stringdistmatrix(string1, string2, method = "jaccard")
+distance
